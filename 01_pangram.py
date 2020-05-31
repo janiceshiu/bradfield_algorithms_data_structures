9760bfb54aa2e7f2c1ac39b1187fe94b21cff7ce
janiceshiu <contrepoint21@gmail.com>
2020-05-31 22:17:06 +0800

    tidy up the docstring

--- 01_pangram.py
+++ 01_pangram.py
@@ -1,10 +1,14 @@
 class Pangram:
   """
-  A pangram is a phrase which contains every letter at least once, such as “the quick brown fox jumps over the lazy dog”.
+  A pangram is a phrase which contains every letter at least once, such as
+  "the quick brown fox jumps over the lazy dog".
 
   Write a function which determines if a given string is a pangram.
 
-  This should be an easy problem, so it’s a good excuse to practice Polya’s method. So, please find at least three different strategies for solving this problem, proceeding through Polya’s method each time. In each case, make a note of how you came to that.
+  This should be an easy problem, so it’s a good excuse to practice Polya’s
+  method. So, please find at least three different strategies for solving
+  this problem, proceeding through Polya’s method each time. In each case,
+  make a note of how you came to that.
   """
 
   def is_pangram_1(self, phrase):
