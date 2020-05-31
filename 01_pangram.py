9ca9c3edd69936362d06703b5252a9e3e54a78ba
janiceshiu <contrepoint21@gmail.com>
2020-05-31 21:53:58 +0800

    tests for Pangram class

--- 01_pangram.py
+++ 01_pangram.py
@@ -16,3 +16,35 @@ class Pangram:
   def is_pangram_3(self, phrase):
     pass
 
+class TestPangram:
+  """
+  Tests the Pangram class
+  """
+
+  @staticmethod
+  def test():
+    phrases = [
+      "The quick brown fox jumps over the lazy dog",
+      "The quick brown fox",
+      "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG",
+      "THE QUICK BrOWN FOX jUMPs over the LaZy Dog...",
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+      "",
+      "    ",
+      ")#(%*)$(*,$^"
+    ]
+
+    expected_result = [True, False, True, True, True, False, False, False]
+
+    p = Pangram()
+
+    is_pangram_1_result = map(p.is_pangram_1, phrases)
+    assert(list(is_pangram_1_result) == expected_result)
+
+    is_pangram_2_result = map(p.is_pangram_2, phrases)
+    assert(list(is_pangram_2_result) == expected_result)
+
+    is_pangram_3_result = map(p.is_pangram_3, phrases)
+    assert(list(is_pangram_3_result) == expected_result)
+
+TestPangram.test()
