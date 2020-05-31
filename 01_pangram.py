f2594914763b7cb741c00460b55aef3338fff79c
janiceshiu <contrepoint21@gmail.com>
2020-05-31 21:53:29 +0800

    skeleton of Pangram class

---
+++ 01_pangram.py
@@ -0,0 +1,18 @@
+class Pangram:
+  """
+  A pangram is a phrase which contains every letter at least once, such as “the quick brown fox jumps over the lazy dog”.
+
+  Write a function which determines if a given string is a pangram.
+
+  This should be an easy problem, so it’s a good excuse to practice Polya’s method. So, please find at least three different strategies for solving this problem, proceeding through Polya’s method each time. In each case, make a note of how you came to that.
+  """
+
+  def is_pangram_1(self, phrase):
+    pass
+
+  def is_pangram_2(self, phrase):
+    pass
+
+  def is_pangram_3(self, phrase):
+    pass
+
