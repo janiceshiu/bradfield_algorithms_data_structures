8d0f6cb4314f06eefc829eafaec07ed91858726d
janiceshiu <contrepoint21@gmail.com>
2020-06-02 22:00:40 +0800

    use a lookup table for converting roman numerals to numbers

--- 01_roman_numerals.py
+++ 01_roman_numerals.py
@@ -44,6 +44,34 @@ class RomanNumerals:
     """
     pass
 
+  def roman_to_number_3(self, string):
+    """
+    Converts a string to roman numerals.
+    Assumes that everything in the string is a valid roman numeral
+    that follows the rules below.
+    Subtraction is present. ie: IX = -1+10 = 9
+    Numbers go bigger to smaller unless subtraction is present.
+    Not all pairs are allowed.
+    * I can precede V or X
+    * X can precede L or C
+    * C can precede D or M
+
+    Inspired by Elliott Jin's suggestion to use the lookup table from the
+    original solution for roman_to_number/2
+    """
+    acc = 0
+    lookup = [
+      ("M", 1000), ("CM", 900), ("D", 500), ("CD", 400), ("C", 100), ("XC", 90),
+      ("L", 50), ("XL", 40), ("X", 10), ("IX", 9), ("V", 5), ("IV", 4), ("I", 1)
+    ]
+
+    for elem in lookup:
+      while string.startswith(elem[0]):
+        acc += elem[1]
+        string = string[len(elem[0]):]
+
+    return acc
+
 class TestRomanNumerals:
   """
   Tests the Roman Numerals class
@@ -51,6 +79,7 @@ class TestRomanNumerals:
 
   def test(self, roman_numerals_class):
     self.test_roman_to_number(roman_numerals_class)
+    self.test_roman_to_number_3(roman_numerals_class)
 
   def test_roman_to_number(self, r):
     strings = ["I", "V", "X", "XIIII", "L", "C", "D", "M", "MMXVI"]
@@ -66,6 +95,14 @@ class TestRomanNumerals:
     expected_result = [1, 4, 5, 9, 10, 14, 50, 100, 500, 1000, 1914, 2016]
 
     result = map(r.roman_to_number_2, strings)
+    assert(list(result) == expected_result)
+
+  def test_roman_to_number_3(self, r):
+    strings = ["I", "IV", "V", "IX", "X", "XIIII", "L", "C", "D", "M", "MCMXIV", "MMXVI"]
+
+    expected_result = [1, 4, 5, 9, 10, 14, 50, 100, 500, 1000, 1914, 2016]
+
+    result = map(r.roman_to_number_3, strings)
     assert(list(result) == expected_result)
 
 test = TestRomanNumerals()
