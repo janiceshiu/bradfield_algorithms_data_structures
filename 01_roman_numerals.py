7ef03f26279255bef10fd47a4ace1ae980530600
janiceshiu <contrepoint21@gmail.com>
2020-06-02 08:45:45 +0800

    add time and space analysis

--- 01_roman_numerals.py
+++ 01_roman_numerals.py
@@ -14,6 +14,13 @@ class RomanNumerals:
     Converts a string to roman numerals.
     Assumes that everything in the string is a valid roman numeral
     Subtraction is not present. ie: IX = 9 does not exist. 9 would be XIIII
+
+    Analysis
+    Time - o(n)
+    * have to iterate through the entire string once. n is the length of the string
+    Space - o(1)
+    * size of lookup table is constant since there is a fixed set of roman numerals
+    * rest are variables that are integers
     """
     lookup = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
 
